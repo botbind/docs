9c2b4c8e38f7150b4235e360b5712f940e328ffa
Gr8z <gr8_boi52@yahoo.com>
2019-09-04 23:25:27 +0500

    added some theme configs

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,18 @@
 module.exports = {
-  title: "Hello VuePress",
-  description: "Just playing around",
+  title: "Developer Docs",
+  description: "Developer documentation for Bot Bind addon developers.",
+  themeConfig: {
+    logo: "./assets/img/logo.png",
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Main Site", link: "http://botbind.com" }
+    ],
+    sidebar: "auto",
+    lastUpdated: true,
+    repo: "botbind/docs",
+    editLinks: true
+  },
   base: "/docs/",
-  dest: "./docs"
+  dest: "./docs",
+  serviceWorker: true
 };
